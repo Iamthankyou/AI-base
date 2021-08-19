e2b85537cff4337c704b22444466c15708bdb95f
Le Quang Duy <quangduynb00@gmail.com>
2021-08-20 02:45:13 +0700

    Hillclimping

---
+++ src/com/company/hillclimping/App.java
@@ -0,0 +1,11 @@
+package com.company.hillclimping;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		HillClimbing climbing = new HillClimbing();
+		climbing.hillClimning();
+		
+	}
+}
