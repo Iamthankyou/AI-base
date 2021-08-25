d1e8a61a36b0a809372270b4d962809965ccc7ce
Le Quang Duy <quangduynb00@gmail.com>
2021-08-25 11:57:04 +0700

    Simulated Annealing for function min/max

---
+++ src/com/company/simulated_annealing/App.java
@@ -0,0 +1,11 @@
+package com.company.simulated_annealing;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		SimulatedAnnealing simulation = new SimulatedAnnealing();
+		simulation.findOptimum();
+		
+	}
+}
