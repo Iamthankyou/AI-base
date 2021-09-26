67baa8a05e4496f8305f3ed0221823d9f3ace93c
Le Quang Duy <quangduynb00@gmail.com>
2021-09-27 01:41:27 +0700

    Minimax without alpha-beta

---
+++ src/com/company/Minimax/App.java
@@ -0,0 +1,8 @@
+package com.company.Minimax;
+
+public class App {
+
+    public static void main(String[] args) {
+       new Game();
+    }
+}
---
+++ src/com/company/Minimax/Constants.java
@@ -0,0 +1,11 @@
+package com.company.Minimax;
+
+public class Constants {
+
+	private Constants(){
+		
+	}
+	
+	public static final int BOARD_SIZE = 3;
+	
+}
